4d8eb706eb58c4822f6dc845991c3a10e6e42254
mazenger <mazen.badawy93@gmail.com>
2024-06-11 17:49:54 +0300

    adding code for prom to collect matrics

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const path = require('path');
+const promClient = require('prom-client');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Collect default metrics
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
+// Create a histogram to measure HTTP request durations
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.5, 1, 2.5, 5, 10]
+});
+
+// Middleware to measure request durations
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ route: req.route ? req.route.path : 'unknown', code: res.statusCode, method: req.method });
+  });
+  next();
+});
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,6 +34,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+// Endpoint to expose metrics
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(promClient.register.metrics());
+});
+
+// Start the server
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
